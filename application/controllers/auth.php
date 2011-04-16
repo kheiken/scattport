cd405f85436cba65c8e52333279bf9ea9852cb9a
Eike Foken <kontakt@eikefoken.de>
2011-04-16 17:42:57 +0200

    Add some comments

--- application/controllers/auth.php
+++ application/controllers/auth.php
@@ -1,11 +1,22 @@
 <?php defined('BASEPATH') || exit("No direct script access allowed");
 
+/**
+ * Authentication controller
+ *
+ * @author Eike Foken <kontakt@eikefoken.de>
+ */
 class Auth extends CI_Controller {
 
+    /**
+     * Shows the index page.
+     */
 	public function index()	{
 		$this->load->view('auth/login');
 	}
 
+	/**
+	 * Logs the user in - or not ;-)
+	 */
 	public function do_login() {
 	    echo "{success: true}";
 	}
