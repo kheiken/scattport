79a2b3bafb8599f3af39344180d5a86108e15f2b
Karsten Heiken <karsten@disposed.de>
2011-04-22 03:24:18 +0200

    Split job logic into model and controller
    There was a weird mix between the both of them.

--- application/controllers/jobs.php
+++ application/controllers/jobs.php
@@ -7,6 +7,7 @@ class Jobs extends CI_Controller {
      */
     public function __construct() {
         parent::__construct();
+		$this->load->model('job');
 
         // load language file
         $this->lang->load(strtolower($this->router->class));
@@ -49,23 +50,20 @@ class Jobs extends CI_Controller {
 				)
 			));
 	}
-	
-	public function listResultsNotSeen() {
-		$query = $this->db->order_by('started_at', 'asc')
-			->get_where('jobs', array('started_by' => $this->session->userdata('user_id'), 'seen' => '0'));
-		$count = $query->num_rows();
-		$jobs = $query->result_array();
-		
-		for($i=0; $i<count($jobs); $i++) {
-			$jobs[$i]['project_name'] = $this->db->select('name')->get_where('projects', array('id' => $jobs[$i]['project_id']))->row()->name;
-		}		
 
+
+	/**
+	 * Get a list of results that the owner has not yet seen.
+	 * @todo not yet verified
+	 */
+	public function get_unseen_results() {
+		$results = $this->job->get_unseen_results();
 		$this->output
 			->set_content_type('application/json')
 			->set_output(json_encode(
 				array(
-					'count' => $count,
-					'jobs' => $jobs
+					'count' => count($results),
+					'results' => $results
 				)
 			));
 	}
