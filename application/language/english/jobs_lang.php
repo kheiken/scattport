2b69148b2f0337942d13a0fb94c2c55f2e1851d3
Karsten Heiken <karsten@disposed.de>
2011-04-22 11:29:47 +0200

    Shorter translation for "failed"

--- application/language/english/jobs_lang.php
+++ application/language/english/jobs_lang.php
@@ -2,7 +2,7 @@
 
 $lang['done'] = "Complete";
 $lang['waiting'] = "Waiting for start...";
-$lang['failed'] = "Computation failed";
+$lang['failed'] = "Failed";
 
 /* End of file projects_lang.php */
 /* Location: ./application/language/english/jobs_lang.php */
