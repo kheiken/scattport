89067dddcfc4445b3d097fd89bf73f21b1d3f78a
Karsten Heiken <karsten@disposed.de>
2011-07-31 18:32:28 +0200

    Spaces to tabs

--- application/controllers/dashboard.php
+++ application/controllers/dashboard.php
@@ -3,17 +3,17 @@
 class Dashboard extends CI_Controller {
 
 	/**
-     * Constructor.
-     */
-    public function __construct() {
-        parent::__construct();
+	* Constructor.
+	 */
+	public function __construct() {
+		parent::__construct();
 		$this->load->model('job');
 		$this->load->model('project');
 		$this->load->model('user');
 
-        // load language file
-        $this->lang->load(strtolower($this->router->class));
-    }
+		// load language file
+		$this->lang->load(strtolower($this->router->class));
+	}
 
 	public function index() {
 		$this->load->view('dashboard');
