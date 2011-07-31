b81794fe7738240acbea593bbbfe1eefc3769d60
Karsten Heiken <karsten@disposed.de>
2011-07-31 16:39:42 +0200

    Add some commonly used buttons to the dashboard

--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -7,19 +7,22 @@
 	</div>
 
 	<div class="box">
-		<h3>Lorem Ipsum.</h3>
-
-		<a class="button add" href="projects/create">Projekt anlegen</a>
-		
-		<p>Er hörte leise Schritte hinter sich. Das bedeutete nichts Gutes. Wer würde ihm schon folgen, spät in der Nacht und dazu noch in dieser engen Gasse mitten im übel beleumundeten Hafenviertel?</p>
-
-		<p>Gerade jetzt, wo er das Ding seines Lebens gedreht hatte und mit der Beute verschwinden wollte! Hatte einer seiner zahllosen Kollegen dieselbe Idee gehabt, ihn beobachtet und abgewartet, um ihn nun um die Früchte seiner Arbeit zu erleichtern?</p>
-
-		<p>Oder gehörten die Schritte hinter ihm zu einem der unzähligen Gesetzeshüter dieser Stadt, und die stählerne Acht um seine Handgelenke würde gleich zuschnappen? Er konnte die Aufforderung stehen zu bleiben schon hören.</p>
-
-		<p>Gehetzt sah er sich um. Plötzlich erblickte er den schmalen Durchgang. Blitzartig drehte er sich nach rechts und verschwand zwischen den beiden Gebäuden. Beinahe wäre er dabei über den umgestürzten Mülleimer gefallen, der mitten im Weg lag.</p>
-
-		<p>Er versuchte, sich in der Dunkelheit seinen Weg zu ertasten und erstarrte: Anscheinend gab es keinen anderen Ausweg aus diesem kleinen Hof als den Durchgang, durch den er gekommen war. Die Schritte wurden lauter und lauter, er sah eine dunkle Gestalt um die Ecke biegen. Fieberhaft irrten seine Augen durch die nächtliche Dunkelheit und suchten einen Ausweg. War jetzt wirklich</p>
+		<h3>Projekte</h3>
+		<p>
+			<a class="button left big" href="projects/create">Projekt anlegen</a><a class="button middle big" href="projects">Projekte verwalten</a><a class="button right big" href="#">Projekt suchen</a>
+		</p>
+	</div>
+	<div class="box">
+		<h3>Berechnungen</h3>
+		<p>
+			<a class="button left big" href="#">Neueste Ergebnisse</a><a class="button middle big" href="#">Laufende Berechnungen</a>
+		</p>
+	</div>
+	<div class="box">
+		<h3>Administration</h3>
+		<p>
+			<a class="button left big" href="#">Server verwalten</a><a class="button middle big" href="#">Programme verwalten</a><a class="button right big" href="#">Benutzer verwalten</a>
+		</p>
 	</div>
 
 </div>
