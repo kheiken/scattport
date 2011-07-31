ceae02ff91b85e88d7a1acd7bd0bef6afbd10420
Karsten Heiken <karsten@disposed.de>
2011-07-31 17:33:50 +0200

    h4 instead of labels for the forms

--- application/views/project/new.php
+++ application/views/project/new.php
@@ -14,14 +14,14 @@
 		<?php echo validation_errors(); ?>
 			<ul>
 				<li>
-					<label>Projektname <span class="req">*</span></label>
+					<h4>Projektname <span class="req">*</span></h4>
 					<div>
 						<input type="text" name="name" class="short text" tabindex="1" value="<?=set_value('name')?>">
 						<?=form_error('name')?>
 					</div>
 				</li>
 				<li>
-					<label>Beschreibung</label><br />
+					<h4>Beschreibung</h4>
 					<label class="note">Eine Beschreibung ist hilfreich, wenn Sie dieses Projekt später für andere Mitarbeiter freigeben möchten.</label>
 					<div>
 						<textarea name="description" rows="6" cols="60" tabindex="2" class="textarea"><?=set_value('description')?></textarea>
@@ -32,7 +32,7 @@
 			<h3>Optionale Angaben</h3>
 			<ul>
 				<li>
-					<label>3D-Modell</label><br />
+					<h4>3D-Modell</h4>
 					<label class="note">Falls ein 3D-Modell für jeden Versuch als Standard definiert werden soll, so können Sie dieses hier hochladen.<br />
 						Es kann weiterhin bei jedem Versuch ein anderes Modell gewählt werden.</label>
 					<div>
@@ -41,7 +41,7 @@
 					</div>
 				</li>
 				<li>
-					<label>Standard-Konfiguration <span class="req">*</span></label><br />
+					<h4>Standard-Konfiguration <span class="req">*</span></h4>
 					<label class="note">Laden Sie eine Konfiguration hoch, die als Vorgabe für alle Versuche verwendet wird.<br />
 						Diese Konfiguration kann bei jedem Versuch geändert werden.</label>
 					<div>
