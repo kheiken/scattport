2d45032a854478affd5a88169b4101509c5fb914
Karsten Heiken <karsten@disposed.de>
2011-08-03 10:50:12 +0200

    Complete form_validation translation

--- application/language/german/form_validation_lang.php
+++ application/language/german/form_validation_lang.php
@@ -1,6 +1,28 @@
 <?php
 
 $lang['unique'] = "Ein Projekt mit diesem Namen existiert bereits";
+$lang['required'] = "&quot;%s&quot; ist ein Pflichtfeld.";
+$lang['isset'] = "&quot;%s&quot; muss einen Wert enthalten.";
+$lang['valid_email'] = "&quot;%s&quot; muss eine gültige E-Mail-Adresse enthalten.";
+$lang['valid_emails'] = "&quot;%s&quot; kann nur gültige E-Mail-Adressen enthalten.";
+$lang['valid_url'] = "&quot;%s&quot; muss eine gültige URL enthalten.";
+$lang['valid_ip'] = "&quot;%s&quot; muss eine gültige IP enthalten.";
+$lang['min_length'] = "&quot;%s&quot; muss mindestens &quot;%s&quot; Zeichen lang sein.";
+$lang['max_length'] = "&quot;%s&quot; darf nicht länger als &quot;%s&quot; Zeichen lang sein.";
+$lang['exact_length'] = "&quot;%s&quot; muss genau &quot;%s&quot; lang sein.";
+$lang['alpha'] = "&quot;%s&quot; darf nur Buchstaben enthalten.";
+$lang['alpha_numeric'] = "&quot;%s&quot; darf nur Buchstaben und/oder Zahlen enthalten.";
+$lang['alpha_dash'] = "&quot;%s&quot; darf nur Buchstaben, Zahlen, Unterstriche und Bindestriche enthalten.";
+$lang['numeric'] = "&quot;%s&quot; darf nur Zahlen enthalten.";
+$lang['is_numeric'] = "&quot;%s&quot; darf nur numerische Zeichen enthalten.";
+$lang['integer'] = "&quot;%s&quot; muss einen ganzzahligen Wert enthalten.";
+$lang['regex_match'] = "&quot;%s&quot; entspricht nicht dem gültigen Format.";
+$lang['matches'] = "&quot;%s&quot; muss mit dem Feld &quot;%s&quot; übereinstimmen.";
+$lang['is_natural'] = "&quot;%s&quot; darf nur positive Zahlen enthalten.";
+$lang['is_natural_no_zero'] = "&quot;%s&quot; darf nur Zahlen größer Null enthalten.";
+$lang['decimal'] = "&quot;%s&quot; muss eine Zahl enthalten.";
+$lang['less_than'] = "&quot;%s&quot; muss einen Wert kleiner als &quot;%s&quot; enthalten.";
+$lang['greater_than'] = "The &quot;%s&quot; muss einen Wert größer als &quot;%s&quot; enthalten.";
 
 /* End of file projects_lang.php */
 /* Location: ./application/language/german/form_validation_lang.php */
