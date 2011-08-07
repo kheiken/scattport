2d9259b8cadb9ec67f3dfab83b95e2764c598876
Eike Foken <kontakt@eikefoken.de>
2011-08-07 19:51:32 +0200

    Translate the settings page

--- application/language/german/settings_lang.php
+++ application/language/german/settings_lang.php
@@ -1,5 +1,11 @@
 <?php
 
+$lang['settings'] = "Einstellungen";
+$lang['tab_personal'] = "Persönliche Daten";
+$lang['tab_settings'] = "Einstellungen";
+
+$lang['projects_sortrecently'] = "Projekte nach Zeitpunkt des letzten Zugriffs sortieren";
+$lang['projects_sortrecently_note'] = "Werden die Projekte nach dem Zeitpunkt des letzten Zugriffs sortiert, &quot;rutschen&quot; die selten verwendeten Projekte in der Liste nach unten.";
 
 /* End of file settings_lang.php */
 /* Location: ./application/language/german/settings_lang.php */
--- application/views/user/settings.php
+++ application/views/user/settings.php
@@ -3,34 +3,34 @@
 <div id="content">
 	<form action="#" method="post">
 		<div class="title">
-			<h2>Einstellungen</h2>
+			<h2><?=lang('settings');?></h2>
 		</div>
 		<ul class="tabs">
-			<li class="active"><a href="#personal">Persönliche Daten</a></li>
-			<li><a href="#settings">Einstellungen</a></li>
+			<li class="active"><a href="#personal"><?=lang('tab_personal');?></a></li>
+			<li><a href="#settings"><?=lang('tab_settings');?></a></li>
 		</ul>
 
 		<div class="tab_container">
 			<div id="personal" class="tab_content">
-				<table>
+				<ul>
 <?
 	foreach($profile_fields as $field):
 ?>
-					<tr>
-						<td><?=$field[1]?></td>
-						<td><input type="<?=$field[2]?>" name="<?=$field[0]?>" class="text"</td>
-					</tr>
+					<li>
+						<h4><?=$field[1]?></h4>
+						<div><input type="<?=$field[2]?>" name="<?=$field[0]?>" class="short text" /></div>
+					</li>
 <?
 	endforeach;
 ?>
-				</table>
+				</ul>
 			</div>
 			<div id="settings" class="tab_content">
 				<ul>
 					<li>
 						<input type="checkbox" id="projects_sortrecently" name="projects_sortrecently" value="1" class="checkbox"/>
-						<label for="projects_sortrecently">Projekte nach Zeitpunkt des letzten Zugriffs sortieren</label><br />
-						<label class="note">Werden die Projekte nach dem Zeitpunkt des letzten Zugriffs sortiert, &quot;rutschen&quot; die selten verwendeten Projekte in der Liste nach unten.</label>
+						<label for="projects_sortrecently"><?=lang('projects_sortrecently');?></label><br />
+						<label class="note"><?=lang('projects_sortrecently_note');?></label>
 					</li>
 				</ul>
 			</div>
