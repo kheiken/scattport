1512b07dc09017bdbe98d66e72a4efef9ee202c4
Eike Foken <kontakt@eikefoken.de>
2011-08-07 21:49:57 +0200

    Add language field to settings page

--- application/views/user/settings.php
+++ application/views/user/settings.php
@@ -1,4 +1,4 @@
-<?php $this->load->view('header'); ?>
+<?php $this->load->view('header');?>
 
 <div id="content">
 	<form action="#" method="post">
@@ -13,20 +13,27 @@
 		<div class="tab_container">
 			<div id="personal" class="tab_content">
 				<ul>
-<?
-	foreach($profile_fields as $field):
-?>
+				<?php foreach($profile_fields as $field):?>
 					<li>
-						<h4><?=$field[1]?></h4>
-						<div><input type="<?=$field[2]?>" name="<?=$field[0]?>" class="short text" /></div>
+						<label><?=$field[1]?></label>
+						<div>
+						  <input type="<?=$field[2]?>" name="<?=$field[0]?>" class="short text" />
+						</div>
 					</li>
-<?
-	endforeach;
-?>
+				<?php endforeach;?>
 				</ul>
 			</div>
 			<div id="settings" class="tab_content">
 				<ul>
+					<li>
+						<label><?=lang('language_select');?></label>
+						<div>
+							<select id="language_select" name="language_select" class="drop">
+								<option value="de">Deutsch</option>
+								<option value="en">Englisch</option>
+							</select>
+						</div>
+					</li>
 					<li>
 						<input type="checkbox" id="projects_sortrecently" name="projects_sortrecently" value="1" class="checkbox"/>
 						<label for="projects_sortrecently"><?=lang('projects_sortrecently');?></label><br />
