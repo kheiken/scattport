1e4adc74fb1358fc3b4800071c925af7f903b836
Eike Foken <kontakt@eikefoken.de>
2011-08-11 15:22:05 +0200

    HTML5!

--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -1,60 +1,63 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!DOCTYPE html>
+<html lang="<?=substr($this->config->item('language'), 0, 2);?>">
 <head>
 
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta charset="utf-8" />
 
-		<title>ScattPort | Login</title>
+<title>ScattPort | <?=_('Login');?></title>
 
-		<?= link_tag('assets/css/login.css'); ?>
-		<?= link_tag('assets/css/form.css'); ?>
+<?= link_tag('assets/css/login.css'); ?>
+<?= link_tag('assets/css/form.css'); ?>
 
-		<?= script_tag('assets/js/minmax.js'); ?>
-		<?= script_tag('https://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js'); ?>
-		<?= script_tag('assets/js/scattport.js'); ?>
-
+<?= script_tag('assets/js/minmax.js'); ?>
+<?= script_tag('https://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js'); ?>
+<?= script_tag('assets/js/scattport.js'); ?>
+<script type="text/javascript">
+	var SITE_URL = '<?=site_url()?>';
+</script>
 </head>
 <body>
 
-	<div id="wrapper">
+<div id="wrapper">
 
-		<div id="box">
-			<h2>Scattport <span class="light">Login</span></h2>
+	<div id="box">
+		<h2>Scattport <span class="light"><?=_('Login');?></span></h2>
 
-			<p>
-				<?php
-				if (isset($error))
-					foreach ($error as $e)
-						echo "<div class=\"error\">" . $e . "</div>";
-				if (isset($notice))
-					foreach ($notice as $n)
-						echo "<div class=\"notice\">" . $n . "</div>";
-				if (isset($success))
-					foreach ($success as $s)
-						echo "<div class=\"success\">" . $s . "</div>";
-				?>
-			</p>
+<?php
+	if (isset($error))
+		foreach ($error as $e)
+			echo "<p class=\"error\">" . $e . "</p>";
+	if (isset($notice))
+		foreach ($notice as $n)
+			echo "<p class=\"notice\">" . $n . "</p>";
+?>
+		<form action="<?= site_url('auth/login') ?>" method="post" name="loginform">
+			<ul>
+				<li>
+					<?=form_label(_('Username'), 'username');?>
+					<div>
+						<input type="text" name="username" id="username" class="text max" value="<?=set_value('username');?>" />
+						<?=form_error('username');?>
+					</div>
+				</li>
+				<li>
+					<?=form_label(_('Password'), 'password');?>
+					<div>
+						<input type="password" name="password" id="password" class="text max" />
+						<?=form_error('password');?>
+					</div>
+				</li>
+				<li>
+					<div>
+						<input type="submit" class="button" name="login" value="<?=_('Log in');?>" />
+					</div>
+				</li>
+			</ul>
+		</form>
 
-			<form action="<?= site_url('auth/login') ?>" method="post" name="loginform">
-				<ul>
-					<li>
-						<label>Benutzername</label>
-						<div><input type="text" name="username" class="text medium" /></div>
-					</li>
-					<li>
-						<label>Passwort</label>
-						<div><input type="password" name="password" class="text medium" /></div>
-					</li>
-					<li>
-						<div>
-							<input type="submit" class="button" name="login" value="Einloggen" />
-						</div>
-					</li>
-				</ul>
-			</form>
-
-			<p><a href="#">Passwort vergessen?</a></p>
-		</div>
+		<p><?=anchor('auth/forgot_password', _('Forgotten password?'));?></p>
 	</div>
+</div>
+
 </body>
 </html>
--- application/views/header.php
+++ application/views/header.php
@@ -1,7 +1,8 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml"><head>
+<!DOCTYPE html>
+<html lang="<?=substr($this->config->item('language'), 0, 2);?>">
+<head>
 
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta charset="utf-8" />
 
 <title>ScattPort</title>
 
@@ -17,6 +18,7 @@
 	var SITE_URL = '<?=site_url()?>';
 </script>
 </head>
+
 <body>
 
 <div id="header">
