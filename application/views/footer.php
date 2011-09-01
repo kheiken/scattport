b9856c3e5f19e13e46764ed5d9ec1c5d3d78b335
Eike Foken <kontakt@eikefoken.de>
2011-09-01 17:38:29 +0200

    Add logos to footer

--- application/views/footer.php
+++ application/views/footer.php
@@ -2,7 +2,7 @@
 <?
 	if ($this->access->isAdmin()):
 ?>
-		<span class="left"><strong><?=_('Administration')?> - </strong>
+		<span class="left"><strong><?=_('Administration')?></strong> -
 			<a href="<?=site_url('admin/settings')?>"><?=_('Global settings')?></a> |
 			<a href="<?=site_url('admin/servers')?>"><?=_('Manage calculation servers')?></a> |
 			<a href="<?=site_url('admin/users')?>"><?=_('Manage users')?></a> |
@@ -11,11 +11,19 @@
 <?
 	endif;
 ?>
-		<span class="right">© 2011 Karsten Heiken.</span>
+
+		<span class="right">
+			<?=anchor('about', _('About'));?> |
+			<?=anchor('legal', _('License'));?>
+		</span>
+		<span class="right">
+			<?=img(array('src' => 'assets/images/iwt.gif', 'width' => 36));?>
+			<?=img(array('src' => 'assets/images/dfg.gif', 'width' => 36));?>
+			<?=img(array('src' => 'assets/images/uni.png', 'width' => 36));?>
+		</span>
 	</div>
 
 </div>
-
 
 </body>
 </html>
