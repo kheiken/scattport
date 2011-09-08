e106201d9d6b54c2139718824a13d02921de7558
Eike Foken <kontakt@eikefoken.de>
2011-09-08 16:00:08 +0200

    Add share model to autoload

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -109,7 +109,7 @@ $autoload['language'] = array();
 |
 */
 
-$autoload['model'] = array('user', 'project');
+$autoload['model'] = array('user', 'project', 'share');
 
 
 /* End of file autoload.php */
