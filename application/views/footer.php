923373e8bbf4a8a275ec9234015bba28ed5ebabb
Eike Foken <kontakt@eikefoken.de>
2011-09-16 01:28:37 +0200

    Remove admin links from footer + customize mini logos

--- application/views/footer.php
+++ application/views/footer.php
@@ -1,26 +1,17 @@
 	<div id="footer">
-<?
-	if ($this->access->isAdmin()):
-?>
-		<span class="left"><strong><?=_('Administration')?></strong> -
-			<a href="<?=site_url('admin/settings')?>"><?=_('Global settings')?></a> |
-			<a href="<?=site_url('admin/servers')?>"><?=_('Manage calculation servers')?></a> |
-			<a href="<?=site_url('admin/users')?>"><?=_('Manage users')?></a> |
-			<a href="<?=site_url('admin/programs')?>"><?=_('Manage programs')?></a>
-		</span>
-<?
-	endif;
-?>
-
 		<span class="right">
+			<?=sprintf(_('Page rendered in %s seconds.'), $this->benchmark->elapsed_time());?>
+		</span>
+		<span class="left">
 			<?=anchor('about', _('About'));?> |
 			<?=anchor('license', _('License'));?>
 		</span>
-		<span class="right">
-			<?=asset('image', 'iwt.gif', array('width' => 36));?>
-			<?=asset('image', 'dfg.gif', array('width' => 36));?>
-			<?=asset('image', 'uni.png', array('width' => 36));?>
-		</span>
+	</div>
+
+	<div id="copyright">
+		<?=image_asset('iwt.png');?>
+		<?=image_asset('uni.png');?>
+		<?=image_asset('dfg.png');?>
 	</div>
 
 </div>
