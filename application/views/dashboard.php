766f8c95f9fb6ffd9caf76882e394f1d2226c629
Eike Foken <kontakt@eikefoken.de>
2011-09-15 18:49:32 +0200

    Add icons to admin buttons in dashboard

--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -5,24 +5,25 @@
 		<div class="title">
 			<h2><?=_('Dashboard');?></h2>
 		</div>
-	
-	
+
+
 		<div class="box">
 			<h3><?=_('Quick actions');?></h3>
 			<p>
-<? 
-	if(count($action_buttons) > 0):
+<?php
+	if (count($action_buttons) > 0):
 		$i = 1;
 		foreach ($action_buttons as $button):
-			if($i == 1 && (count($action_buttons) == 1))
+			if ($i == 1 && count($action_buttons) == 1) {
 				$button['class'] = '';
-			elseif($i == count($action_buttons))
+			} else if ($i == count($action_buttons)) {
 				$button['class'] = 'right';
-			elseif($i == 1)
+			} else if ($i == 1) {
 				$button['class'] = 'left';
-			else
+			} else {
 				$button['class'] = 'middle';
-?><a class="button <?=$button['class'];?> big" href="<?=$button['target'];?>"><strong><?=image_asset($button['icon']);?></strong><br /><?=$button['text'];?></a><? 
+			}
+?><a class="button <?=$button['class'];?> big" href="<?=$button['target'];?>"><strong><?=image_asset($button['icon']);?></strong><br /><?=$button['text'];?></a><?
 			$i++;
 		endforeach;
 	endif;
@@ -32,19 +33,20 @@
 		<div class="box">
 			<h3><?=_('Recent events');?></h3>
 			<p>
-<? 
-	if(count($recent_buttons) > 0):
+<?
+	if (count($recent_buttons) > 0):
 		$i = 1;
 		foreach ($recent_buttons as $button):
-			if($i == 1 && (count($recent_buttons) == 1))
+			if ($i == 1 && count($recent_buttons) == 1) {
 				$button['class'] = '';
-			elseif($i == count($recent_buttons))
+			} else if ($i == count($recent_buttons)) {
 				$button['class'] = 'right';
-			elseif($i == 1)
+			} else if ($i == 1) {
 				$button['class'] = 'left';
-			else
+			} else {
 				$button['class'] = 'middle';
-?><a class="button <?=$button['class'];?> big" href="<?=$button['target'];?>" title="<?=$button['title'];?>"><strong><?=$button['count'];?></strong><br /><?=$button['text'];?></a><? 
+			}
+?><a class="button <?=$button['class'];?> big" href="<?=$button['target'];?>" title="<?=$button['title'];?>"><strong><?=$button['count'];?></strong><br /><?=$button['text'];?></a><?
 			$i++;
 		endforeach;
 	endif;
@@ -54,7 +56,9 @@
 		<div class="box">
 			<h3><?=_('Administration');?></h3>
 			<p>
-				<a class="button left big" href="#"><?=_('Manage servers');?></a><a class="button middle big" href="<?=site_url('admin/programs');?>"><?=_('Manage programs');?></a><a class="button right big" href="<?=site_url('admin/users');?>"><?=_('Manage users');?></a>
+				<a class="button left big" href="<?=site_url('admin/servers');?>"><strong><?=image_asset('icons-big/server.png');?></strong><br /><?=_('Manage servers');?>
+				</a><a class="button middle big" href="<?=site_url('admin/programs');?>"><strong><?=image_asset('icons-big/application.png');?></strong><br /><?=_('Manage programs');?>
+				</a><a class="button right big" href="<?=site_url('admin/users');?>"><strong><?=image_asset('icons-big/user.png');?></strong><br /><?=_('Manage users');?></a>
 			</p>
 		</div>
 	</div>
