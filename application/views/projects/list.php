aebde4aa79d0468d0e2255295a10a82df378b9c9
Eike Foken <kontakt@eikefoken.de>
2011-09-20 22:32:54 +0200

    Show "you" in projects list

--- application/views/projects/list.php
+++ application/views/projects/list.php
@@ -19,11 +19,23 @@
 			</thead>
 			<tbody>
 <?php
-	foreach($projects as $project):
+	foreach ($projects as $project):
 ?>
 				<tr>
 					<td><a href="<?=site_url('projects/detail/' . $project['id'] . '?active_project=' . $project['id']);?>"><abbr title="<?=$project['name'] . "\n\n" . $project['description'];?>"><?=$project['mediumname'];?></abbr></a></td>
-					<td><a href="<?=site_url('users/profile/' . $project['username']);?>" title="<?=_('Show profile');?>"><?=$project['firstname'] . ' ' . $project['lastname'];?></a></td>
+					<td>
+<?php
+		if ($project['owner'] == $this->access->profile()->id):
+?>
+						<?=_('You');?>
+<?php
+		else:
+?>
+						<a href="<?=site_url('users/profile/' . urlencode($project['username']));?>" title="<?=_('Show profile');?>"><?=$project['firstname'] . ' ' . $project['lastname'];?></a>
+<?php
+		endif;
+?>
+					</td>
 					<td><span class="active"><?=_('Successfully finished');?></span></td>
 					<td><a href="#"><?=_('Show results');?></a> | <?=anchor('projects/delete/' . $project['id'], _('Delete'));?>
 					</td>
