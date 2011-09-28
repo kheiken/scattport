985be7a7fb6d68cce67e435c75e538fcdb3a71d5
Eike Foken <kontakt@eikefoken.de>
2011-09-28 23:41:29 +0200

    Add fugue copyright to about page

--- application/views/about/about.php
+++ application/views/about/about.php
@@ -19,6 +19,8 @@
 				<li>Karsten Heiken, 2011</li>
 				<li>Eike Foken, 2011</li>
 			</ul>
+			<h4>Icons</h4>
+			<p><a href="http://p.yusukekamiyamane.com">Fugue Icons</a> v3.1.8 Copyright by Yusuke Kamiyamane</p>
 		</p>
 	</div>
 
