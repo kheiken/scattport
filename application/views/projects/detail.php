6fd88abc2466c4df8c47540915b47932a9eeec2b
Eike Foken <kontakt@eikefoken.de>
2011-09-28 18:00:43 +0200

    Hide link for editing experiments if a job exists

--- application/views/projects/detail.php
+++ application/views/projects/detail.php
@@ -96,11 +96,9 @@
 						<a href="<?=site_url('experiments/create/' . $project['id'] . '/' . $experiment['id']);?>" title="<?=sprintf(_('Copy experiment &quot;%s&quot;'), $experiment['name']);?>"><?=_('Copy');?></a>
 <?php
 			if ($experiment['creator_id'] == $this->access->profile()->id || $this->access->isAdmin()):
-?>
-						| <a href="<?=site_url('experiments/edit/' . $experiment['id']);?>" title="<?=sprintf(_('Edit this experiment'), $experiment['name']);?>"><?=_('Edit');?></a>
-<?php
 				if ($job['css'] == 'closed' || $job['css'] == ''):
 ?>
+						| <a href="<?=site_url('experiments/edit/' . $experiment['id']);?>" title="<?=sprintf(_('Edit this experiment'), $experiment['name']);?>"><?=_('Edit');?></a>
 						| <a href="javascript:deleteConfirm('<?=site_url('experiments/delete/' . $experiment['id']);?>');" title="<?=sprintf(_('Delete this experiment'), $experiment['name']);?>"><?=_('Delete');?></a>
 <?php
 				endif;
