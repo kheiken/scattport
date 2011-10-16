3b208bdbdae72b99638e6b15060d604df8193b03
Karsten Heiken <karsten@disposed.de>
2011-10-16 16:56:27 +0200

    Add local fallback jQuery
    If we can not download jQuery from Google's CDN, we
    use a local copy.

--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -11,7 +11,9 @@
 <?=css_asset('form.css');?>
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js" language="javascript" type="text/javascript"></script>
-
+<script type="text/javascript">
+if (typeof jQuery == 'undefined') document.write(unescape("%3Cscript src='<?=base_url('/assets/js/jquery-1.6.2.min.js');?>' type='text/javascript'%3E%3C/script%3E"));
+</script>
 <?=js_asset('minmax.js');?>
 <?=js_asset('scattport.js');?>
 
--- application/views/header.php
+++ application/views/header.php
@@ -12,7 +12,9 @@
 <?=css_asset('form.css');?>
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js" language="javascript" type="text/javascript"></script>
-
+<script type="text/javascript">
+if (typeof jQuery == 'undefined') document.write(unescape("%3Cscript src='<?=base_url('/assets/js/jquery-1.6.2.min.js');?>' type='text/javascript'%3E%3C/script%3E"));
+</script>
 <?=js_asset('jquery.editinplace.js');?>
 <?=js_asset('jquery.tablednd.js');?>
 <?=js_asset('jquery.alerts.js');?>
